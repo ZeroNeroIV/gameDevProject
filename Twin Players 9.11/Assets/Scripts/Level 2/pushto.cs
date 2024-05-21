88890fea8ac73a24ae516d798cea3bbcd5bba71b
Mohammad Al-Qaruti <117480808+m7md-004@users.noreply.github.com>
2024-05-21 16:08:00 +0300

    cone and twoer done moving

--- Twin Players 9.11/Assets/Scripts/Level 2/pushto.cs
+++ Twin Players 9.11/Assets/Scripts/Level 2/pushto.cs
@@ -7,27 +7,16 @@ namespace Level_2
 {
     public class PushTo : MonoBehaviour
     {
-        public PushTo1 one;
-        public PushTo2 two;
-        public PushTo3 three;
-        public bool good=false;
-        //faisal will 
+        
         // Start is called before the first frame update
         void Start()
         {
-            one=GameObject.Find($"{gameObject.name}/Arch1/Cone1").GetComponent<PushTo1>();
-            two=GameObject.Find($"{gameObject.name}/Arch2/Cone2").GetComponent<PushTo2>();
-            three=GameObject.Find($"{gameObject.name}/Arch3/Cone3").GetComponent<PushTo3>();
+            
         }
         // Update is called once per frame
         void Update()
         {
-            if(one.done && two.done && three.done){
-                good=true;
-            }
-            else{
-                good=false;
-            }
+            
         }
 
     }

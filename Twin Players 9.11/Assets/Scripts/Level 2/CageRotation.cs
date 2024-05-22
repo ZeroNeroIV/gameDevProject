9eb7fdfa32fd23c61f0acfca84cd8bd5f49fd79d
ZeroNeroIV <55910337.a@gmail.com>
2024-05-22 12:00:54 +0300

    .

--- Twin Players 9.11/Assets/Scripts/Level 2/CageRotation.cs
+++ Twin Players 9.11/Assets/Scripts/Level 2/CageRotation.cs
@@ -4,7 +4,7 @@ namespace Level_2
 {
     public class CageRotation : MonoBehaviour
     {
-        private readonly Vector3 _cageRotation = new(4f, 7f, 2.5f);
+        private readonly Vector3 _cageRotation = new(8f, 14f, 5f);
         private void Update() => transform.Rotate(_cageRotation * Time.deltaTime);
     }
 }
